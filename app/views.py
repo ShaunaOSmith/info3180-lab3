2a899490b14973ba65df51a85cb276bae2997e37
Shauna Smith <smithshauna2014@gmail.com>
2024-02-28 10:04:00 -0500

    Added my codes for lab3

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from app.forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -15,7 +17,28 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Shauna Smith")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        msg = Message(subject, 
+                      sender = (name, email),
+                      recipients=["your-email@example.com"])
+        msg.body = f"From: {name} <{email}>\n\n{message}"
+
+        mail.send(msg)
+
+        flash('Your email was successfully sent!', 'success')
+        return redirect(url_for('home'))
+    """Render website's contact page."""
+    return render_template('contact.html', form=form)
 
 
 ###
